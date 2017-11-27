f87389ea2857c422b7107c770ce5399aaad028c4
Benjamin <ben@npmjs.com>
2017-11-26 18:48:06 -0800

    feat: get things working using Runtime.executionContextDestroyed

--- README.md
+++ README.md
@@ -14,13 +14,13 @@ The above example will collect coverage for `foo.js` using v8's profiler.
 
 TODO:
 
-- [ ] write logic for converting v8 coverage output to [Istanbul Coverage.json format](https://github.com/gotwarlost/istanbul/blob/master/coverage.json.md).
+- [x] write logic for converting v8 coverage output to [Istanbul Coverage.json format](https://github.com/gotwarlost/istanbul/blob/master/coverage.json.md).
 - [ ] talk to Node.js project about silencing messages:
 
    > `Debugger listening on ws://127.0.0.1:56399/e850110a-c5df-41d8-8ef2-400f6829617f`.
 
-- [ ] figure out why `detailed` mode does not appear to be working.
-- [ ] figure out a better way to determine that all processes in event loop
+- [x] figure out why `detailed` mode does not appear to be working.
+- [x] figure out a better way to determine that all processes in event loop
    have terminated (except the inspector session).
-- [ ] process.exit() can't perform an async operation; how can we track coverage
+- [x] process.exit() can't perform an async operation; how can we track coverage
   for scripts that exit?
--- bin/c8.js
+++ bin/c8.js
@@ -1,10 +1,58 @@
 #!/usr/bin/env node
 
 const argv = require('yargs').parse()
+const CRI = require('chrome-remote-interface')
+const getPort = require('get-port');
 const foreground = require('foreground-child')
-const sw = require('spawn-wrap')
+const waitTillPortOpen = require('wait-till-port-open')
 
-if (argv._.length) {
-  sw([require.resolve('./wrap')])
-  foreground(process.argv.slice(2))
+getPort().then(async port => {
+  foreground(
+    ['node', `--inspect-brk=${port}`].concat(process.argv.slice(2)),
+    (done) => {
+      console.info('actually got here')
+    }
+  )
+  try {
+    await waitTillPortOpen(port)
+    const client = await CRI({port: port})
+
+    const {Debugger, Runtime, Profiler} = client
+    await Runtime.runIfWaitingForDebugger()
+    await Runtime.enable()
+    await Profiler.enable()
+    await Profiler.startPreciseCoverage({callCount: true, detailed: true})
+    await Debugger.enable()
+    await Debugger.paused()
+    await Debugger.resume()
+
+    client.on('event', async (message) => {
+      // console.info(message)
+      if (message.method === 'Runtime.executionContextDestroyed') {
+        await outputCoverage(Profiler)
+        client.close()
+      }
+    })
+
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+})
+
+async function outputCoverage (Profiler) {
+  const IGNORED_PATHS = [
+    /\/bin\/wrap.js/,
+    /\/node_modules\//,
+    /node-spawn-wrap/
+  ]
+  let {result} = await Profiler.takePreciseCoverage()
+  result = result.filter(coverage => {
+    for (var ignored, i = 0; (ignored = IGNORED_PATHS[i]) !== undefined; i++) {
+      if (ignored.test(coverage.url)) return false
+    }
+    if (!/^\//.test(coverage.url)) return false
+    else return true
+  })
+  console.log(JSON.stringify(result, null, 2))
 }
