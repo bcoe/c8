a7c53822cdeed356a30179486cdf59256e4b894d
bcoe <bencoe@google.com>
2019-05-01 23:33:49 -0700

    make mininum node version a variable

--- bin/c8.js
+++ bin/c8.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 'use strict'
 
+const minNode = `10.12.0`
 const semver = require('semver')
 
 /* c8 ignore next 3 */
-if (semver.lt(process.version, '10.12.0')) {
-  console.warn(`c8 requires Node v10.12.0 or greater, found ${process.version}`)
+if (semver.lt(process.version, minNode)) {
+  console.warn(`c8 requires Node v${minNode} or greater, found ${process.version}`)
 } else {
   require('./run')
 }
