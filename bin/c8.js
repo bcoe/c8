a4e1ea0ef93952ef1f7d6fa1a888d3b58c5bc5c4
Benjamin Coe <bencoe@google.com>
2019-05-02 17:03:07 -0700

    fix: switch to mkdirp for Node 8

--- bin/c8.js
+++ bin/c8.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 'use strict'
 
-const fs = require('fs')
 const foreground = require('foreground-child')
 const { outputReport } = require('../lib/commands/report')
+const mkdirp = require('mkdirp')
 const { promisify } = require('util')
 const rimraf = require('rimraf')
 const {
@@ -24,7 +24,8 @@ let argv = buildYargs().parse(instrumenterArgs)
     if (argv.clean) {
       await promisify(rimraf)(argv.tempDirectory)
     }
-    await promisify(fs.mkdir)(argv.tempDirectory, { recursive: true })
+    // allow c8 to run on Node 8 (coverage just won't work).
+    await promisify(mkdirp)(argv.tempDirectory)
 
     process.env.NODE_V8_COVERAGE = argv.tempDirectory
     foreground(hideInstrumenterArgs(argv), async (done) => {
