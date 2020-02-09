f85918e808429ac707f03dce4cc3042de60b27f7
Fabian Wiles <fabian.wiles@gmail.com>
2020-02-09 15:20:12 +1300

    feat: adds TypeScript definitions
    fixes #195

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+export type Watermark = [number, number];
+
+export declare class Report {
+    constructor(opts: {
+        exclude?: any[],
+        include?: any[],
+        reporter: string[],
+        reportsDirectory?: string,
+        tempDirectory?: string,
+        watermarks?: Partial<{
+            statements: Watermark;
+            functions: Watermark;
+            branches: Watermark;
+            lines: Watermark;
+        }>,
+        omitRelative?: boolean,
+        wrapperLength?: number,
+        resolve?: string,
+        all?: boolean,
+    })
+
+    run(): Promise<void>;
+}
