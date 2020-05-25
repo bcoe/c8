7e53a0ecf93eda5467e2a8ac7d33cf8d6af84cf1
Joseph Mordetsky <jmordetsky@gmail.com>
2020-05-25 15:44:21 -0400

    feat: support for instrumenting files outside of current working directory

---
+++ test/fixtures/report/allowExternal.js
@@ -0,0 +1,1 @@
+require("../multidir1/file1")
---
+++ test/fixtures/source-maps/padded.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var loaded_1 = require("./loaded");
+console.log(loaded_1["default"](0));
+console.log(loaded_1["default"](1));
+console.log(loaded_1["default"](-1));
+//# sourceMappingURL=padded.js.map
+
+
+//ew extra whitespace ^
