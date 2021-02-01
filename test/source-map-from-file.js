3f12dd4cd4b903b396c60c9c6d76cdf990ec6cbe
Benjamin E. Coe <bencoe@google.com>
2021-02-01 15:52:48 -0800

    feat(all): handle base64 inline source maps (#283)
    Pulls in Source Map handling logic from Node.js

--- test/source-map-from-file.js
+++ test/source-map-from-file.js
@@ -1,14 +1,19 @@
 /* global describe, it */
 const getSourceMapFromFile = require('../lib/source-map-from-file')
 const assert = require('assert')
-const path = require('path')
+const { readFileSync } = require('fs')
 describe('source-map-from-file', () => {
   it('should parse source maps from compiled targets', () => {
     const sourceMap = getSourceMapFromFile('./test/fixtures/all/ts-compiled/main.js')
-    assert.strictEqual(sourceMap, ['test', 'fixtures', 'all', 'ts-compiled', 'main.js.map'].join(path.sep))
+    const expected = JSON.parse(readFileSync(require.resolve('./fixtures/all/ts-compiled/main.js.map'), 'utf8'))
+    assert.deepStrictEqual(sourceMap, expected)
   })
   it('should handle extra whitespace characters', () => {
     const sourceMap = getSourceMapFromFile('./test/fixtures/source-maps/padded.js')
-    assert.strictEqual(sourceMap, ['test', 'fixtures', 'source-maps', 'padded.js.map'].join(path.sep))
+    assert.deepStrictEqual(sourceMap, { version: 3 })
+  })
+  it('should support base64 encoded inline source maps', () => {
+    const sourceMap = getSourceMapFromFile('./test/fixtures/source-maps/inline.js')
+    assert.strictEqual(sourceMap.version, 3)
   })
 })
