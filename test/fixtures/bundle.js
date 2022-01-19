4d03a0f7fc087f60adc73a4492c789da88e7bbfb
张博涵 <zhangbohan.0@bytedance.com>
2022-01-19 16:00:46 +0800

    feat: exclude unexpected file in sourcemap result

---
+++ test/fixtures/bundle.js
@@ -0,0 +1,58 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __reExport = (target, module2, copyDefault, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
+        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toESM = (module2, isNodeMode) => {
+  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", !isNodeMode && module2 && module2.__esModule ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
+};
+
+// node_modules/path-exists/index.js
+var require_path_exists = __commonJS({
+  "node_modules/path-exists/index.js"(exports, module2) {
+    "use strict";
+    var fs = require("fs");
+    var { promisify } = require("util");
+    var pAccess = promisify(fs.access);
+    module2.exports = async (path) => {
+      try {
+        await pAccess(path);
+        return true;
+      } catch (_) {
+        return false;
+      }
+    };
+    module2.exports.sync = (path) => {
+      try {
+        fs.accessSync(path);
+        return true;
+      } catch (_) {
+        return false;
+      }
+    };
+  }
+});
+
+// index.js
+var { sync } = require_path_exists();
+try {
+  sync({
+    cwd: require.resolve("aaaa")
+  });
+} catch (err) {
+  console.log("reachable");
+}
+//# sourceMappingURL=bundle.js.map
