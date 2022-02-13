99fd9602b492350e3abbda331fbe17526210f8bf
XhmikosR <xhmikosr@gmail.com>
2022-02-13 16:22:17 +0200

    chore: remove pre-Node.js 10.x code and tests

--- bin/c8.js
+++ bin/c8.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict'
 
-const foreground = require('foreground-child')
-const { outputReport } = require('../lib/commands/report')
-const { promises } = require('fs')
+const { mkdir } = require('fs').promises
 const { promisify } = require('util')
+const foreground = require('foreground-child')
 const rimraf = require('rimraf')
+const { outputReport } = require('../lib/commands/report')
 const {
   buildYargs,
   hideInstrumenteeArgs,
@@ -21,18 +21,11 @@ async function run () {
   ].indexOf(argv._[0]) !== -1) {
     argv = buildYargs(true).parse(process.argv.slice(2))
   } else {
-    // fs.promises was not added until Node.js v10.0.0, if it doesn't
-    // exist, assume we're Node.js v8.x and skip coverage.
-    if (!promises) {
-      foreground(hideInstrumenterArgs(argv))
-      return
-    }
-
     if (argv.clean) {
       await promisify(rimraf)(argv.tempDirectory)
     }
 
-    await promises.mkdir(argv.tempDirectory, { recursive: true })
+    await mkdir(argv.tempDirectory, { recursive: true })
     process.env.NODE_V8_COVERAGE = argv.tempDirectory
     foreground(hideInstrumenterArgs(argv), async (done) => {
       try {
