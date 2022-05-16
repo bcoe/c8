dc87b08cf1a5043536a2feab1b46883cb538ef5b
bencoe@google.com <bencoe@google.com>
2022-05-16 00:09:03 +0000

    chore: update snapshots

--- test/integration.js
+++ test/integration.js
@@ -639,7 +639,7 @@ describe('c8', () => {
         '--extension=.js',
         '--extension=.special',
         '--temp-directory=tmp/extension',
-        '--clean=false',
+        '--clean=true',
         nodePath,
         require.resolve('./fixtures/custom-ext.special')
       ])
