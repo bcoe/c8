6ff250fa443807ed4ee179d1b2364643d8b46f63
Trevor D. McKeown <tmckeown@gmail.com>
2024-06-14 11:54:35 -0400

    Additional Configuration File Support (#436)
    feat: Adding additional configuration file options
    test: unit test for config file detection
    test: unit test for config files pass via the cli
    test: unit test case for early exit of hideInstrumenteeArgs
    test: unit test for Node.js argument handling
    fix: bug in spawning of c8 process that dropped coverage significantly

---
+++ test/fixtures/config/.c8rc.cjs
@@ -0,0 +1,11 @@
+const config = {
+  "reporter": [
+    "html",
+    "text"
+  ],
+  "lines": 32,
+  "branches": "72",
+  "statements": "65"
+}
+
+module.exports = config
---
+++ test/fixtures/config/.c8rc.js
@@ -0,0 +1,11 @@
+const config = {
+  "reporter": [
+    "html",
+    "text"
+  ],
+  "lines": 22,
+  "branches": "72",
+  "statements": "65"
+}
+
+module.exports = config
